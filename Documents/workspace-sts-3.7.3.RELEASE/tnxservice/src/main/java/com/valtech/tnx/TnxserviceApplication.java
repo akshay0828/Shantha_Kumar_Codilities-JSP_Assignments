b03ed7d011773262759b8da4e09fe86af1d90d5f
Ritesh Kumar Singh <Ritesh.Singh@valtech.com>
2022-10-20 17:49:56 +0530

    adding tnx

---
+++ Documents/workspace-sts-3.7.3.RELEASE/tnxservice/src/main/java/com/valtech/tnx/TnxserviceApplication.java
@@ -0,0 +1,13 @@
+package com.valtech.tnx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnxserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnxserviceApplication.class, args);
+	}
+
+}
---
+++ Documents/workspace-sts-3.7.3.RELEASE/tnxservice/src/main/java/com/valtech/tnx/service/TxService.java
@@ -0,0 +1,17 @@
+package com.valtech.tnx.service;
+
+import java.util.List;
+
+import com.valtech.tnx.entity.Tx;
+
+public interface TxService {
+
+	Tx createTx(Tx tx);
+
+	Tx updateTx(Tx tx);
+
+	Tx getTx(long id);
+
+	List<Tx> getAllTx();
+
+}
